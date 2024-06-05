92d217732229b080c7301d0a067d00c55a560e64
Aaron Martinez <aarondev98@gmail.com>
2024-06-05 18:30:42 -0400

    for loop fix

--- main.py
+++ main.py
@@ -39,83 +39,83 @@ response_sesiones = requests.get(sesiones_url)
 r = response_sesiones.json()
 
 id_sesion = str(r['results'][0]['sesionId']) 
-
+print(id_sesion)
 # ******************** validacion sesion reciente. ***********
 if id_sesion == id_last_sesion:
     print('funciono', id_sesion, id_last_sesion)
+else:
+    for id_sesion in range(int(id_last_sesion) + 1, int(id_sesion)+1):
+        print('ok')
+        # ********** GET ID ASISTENCIA ************
+        sesion_url = f"https://www.diputadosrd.gob.do/sil/api/asistencia/sesion/?sesionId={id_sesion}"
 
-for id_sesion in range(int(id_last_sesion) + 1, int(id_sesion)):
+        response_resumen_asistencia = requests.get(sesion_url)
+        r = response_resumen_asistencia.json()
+        id_asistencia = str(r['id'])
 
-    # ********** GET ID ASISTENCIA ************
-    sesion_url = f"https://www.diputadosrd.gob.do/sil/api/asistencia/sesion/?sesionId={id_sesion}"
-
-    response_resumen_asistencia = requests.get(sesion_url)
-    r = response_resumen_asistencia.json()
-    id_asistencia = str(r['id'])
-
-    resumen_sesion.append({
-        'id_asistencia': r['id'],
-        'id_sesion': r['sesion']['id'],
-        'numero': r['sesion']['numero'],
-        'lugar': r['sesion']['lugar'],
-        'fecha': r['fecha'],
-        'cantidadDelegados': r['cantidadAsistencia']['cantidadDelegados'],
-        'cantidadPresentes': r['cantidadAsistencia']['cantidadPresentes'],
-        'cantidadAusentes': r['cantidadAsistencia']['cantidadAusentes'],
-        'totalLegisladores': r['cantidadAsistencia']['totalLegisladores'],
-        'source': 'https://www.diputadosrd.gob.do/sil/sesion/' + str(id_sesion)
-    })
-    # formatted_data.append({'resumen_sesion':resumen_sesion})
-
-    # *********************** GET LISTA ASISTENCIA ***************************************
-
-    for x in range(19):
-        x_str = str(x + 1)
-        asistencia_url = f'https://www.diputadosrd.gob.do/sil/api/asistencia/legisladores/?page={x_str}&id={id_asistencia}'
-
-        response_asistencia = requests.get(asistencia_url)
-        resp = response_asistencia.json()
-
-        for result in resp['results']:
-            legislador = result['legislador']
-            legislador_id = legislador['legisladorId']
-            nombre_completo = legislador['nombreCompleto']
-            presente = result['presente']
-            excusa = result['excusa']
-
-            asistencia.append({
-                'legisladorId': legislador_id,
-                'nombreCompleto': nombre_completo,
-                'presente': presente,
-                'excusa': excusa
-            })
-
-    formatted_data.append({
-        'sesion': ({
-            'resumen_sesion': resumen_sesion,
-            'asistencia': asistencia
+        resumen_sesion.append({
+            'id_asistencia': r['id'],
+            'id_sesion': r['sesion']['id'],
+            'numero': r['sesion']['numero'],
+            'lugar': r['sesion']['lugar'],
+            'fecha': r['fecha'],
+            'cantidadDelegados': r['cantidadAsistencia']['cantidadDelegados'],
+            'cantidadPresentes': r['cantidadAsistencia']['cantidadPresentes'],
+            'cantidadAusentes': r['cantidadAsistencia']['cantidadAusentes'],
+            'totalLegisladores': r['cantidadAsistencia']['totalLegisladores'],
+            'source': 'https://www.diputadosrd.gob.do/sil/sesion/' + str(id_sesion)
         })
-    })
+        # formatted_data.append({'resumen_sesion':resumen_sesion})
 
-    formatted_data = [
-        {
-            'id': str(uuid.uuid4()),
-            'sesion': {
+        # *********************** GET LISTA ASISTENCIA ***************************************
+
+        for x in range(19):
+            x_str = str(x + 1)
+            asistencia_url = f'https://www.diputadosrd.gob.do/sil/api/asistencia/legisladores/?page={x_str}&id={id_asistencia}'
+
+            response_asistencia = requests.get(asistencia_url)
+            resp = response_asistencia.json()
+
+            for result in resp['results']:
+                legislador = result['legislador']
+                legislador_id = legislador['legisladorId']
+                nombre_completo = legislador['nombreCompleto']
+                presente = result['presente']
+                excusa = result['excusa']
+
+                asistencia.append({
+                    'legisladorId': legislador_id,
+                    'nombreCompleto': nombre_completo,
+                    'presente': presente,
+                    'excusa': excusa
+                })
+
+        formatted_data.append({
+            'sesion': ({
                 'resumen_sesion': resumen_sesion,
                 'asistencia': asistencia
+            })
+        })
+
+        formatted_data = [
+            {
+                'id': str(uuid.uuid4()),
+                'sesion': {
+                    'resumen_sesion': resumen_sesion,
+                    'asistencia': asistencia
+                }
             }
-        }
-    ]
+        ]
 
-    # ************* Cosmos DB handling ************
+        # ************* Cosmos DB handling ************
 
-    for item in formatted_data:
-        try:
-            container.create_item(body=item)
-            print("Item created successfully")
-        except exceptions.CosmosResourceExistsError:
-            print("Item already exists")
-        except exceptions.CosmosHttpResponseError as e:
-            print(f"An error occurred: {e.message}")
+        for item in formatted_data:
+            try:
+                container.create_item(body=item)
+                print("Item created successfully")
+            except exceptions.CosmosResourceExistsError:
+                print("Item already exists")
+            except exceptions.CosmosHttpResponseError as e:
+                print(f"An error occurred: {e.message}")
 
-    print("Upload complete")
+        print("Upload complete")
